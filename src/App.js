9a8f3b02376a6d147d0959c6f6e320ebbe8f0f71
Sexy-Relief <enjoybeawall@gmail.com>
2022-12-27 21:31:34 +0900

    deletion complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { BrowserRouter as Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
 import ImageGetter from "./components/get-images.component";
 import MyImage from "./components/get-myimage.component";
 
@@ -17,9 +13,6 @@ function App() {
                 <Navbar />
                 <br />
                 <Route path="/" exact component={ImageGetter} />
-                <Route path="/edit/:id" component={EditExercise} />
-                <Route path="/create" component={CreateExercise} />
-                <Route path="/user" component={CreateUser} /> 
                 <Route path="/myimage" component={MyImage} />
             </div>
         </Routes>
